e8e51f4dfc7499a1777126f301c56932139faffe
tomas-engineer <tomas.heij2006@gmail.com>
2025-09-08 12:08:28 +0200

    Basis opzet

--- sancties/next.config.ts
+++ sancties/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/index",
+      },
+    ];
+  },
   /* config options here */
 };
 
