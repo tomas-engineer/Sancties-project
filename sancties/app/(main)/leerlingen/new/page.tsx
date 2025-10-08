528bb9839c8ffc2c43ae0bfefd3e656d5e7d6918
tomas-engineer <tomas.heij2006@gmail.com>
2025-10-08 10:15:21 +0200

    Nieuwe leerling en sanctie paginas gemaakt

--- sancties/app/(main)/leerlingen/new/page.tsx
+++ sancties/app/(main)/leerlingen/new/page.tsx
@@ -1,3 +1,42 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+
 export default function New() {
-  return <h1>New</h1>;
+  const [error, setError] = useState<string | boolean>(false);
+
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault();
+    setError(false);
+
+    const form = (e.target as HTMLElement).closest("form");
+    const input = form?.querySelector("input") as HTMLInputElement;
+
+    const name = input.value;
+    if (!name) return setError("Een of meerdere velden zijn niet ingevuld");
+
+    console.log(name);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <form
+        className="flex flex-row items-center justify-center gap-2 mb-3"
+        onSubmit={submitForm}
+      >
+        <input
+          type="text"
+          className="form-control w-fit"
+          id="usernameInput"
+          placeholder="Naam"
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Aanmaken
+        </button>
+      </form>
+
+      {error && <span className="text-red-400">{error}</span>}
+    </div>
+  );
 }
--- sancties/app/(main)/sancties/new/page.tsx
+++ sancties/app/(main)/sancties/new/page.tsx
@@ -1,3 +1,57 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+
 export default function New() {
-  return <h1>New</h1>;
+  const [error, setError] = useState<string | boolean>(false);
+
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault();
+    setError(false);
+
+    const form = (e.target as HTMLElement).closest("form");
+
+    const usernameInput = form?.querySelector(
+      "#usernameInput"
+    ) as HTMLInputElement;
+    const niveauInput = form?.querySelector("#niveauInput") as HTMLInputElement;
+
+    const name = usernameInput.value;
+    const niveau = niveauInput.value;
+
+    if (!name || !niveau)
+      return setError("Een of meerdere velden zijn niet ingevuld");
+
+    console.log(name);
+    console.log(niveau);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <form
+        className="flex flex-row items-center justify-center gap-2 mb-3"
+        onSubmit={submitForm}
+      >
+        <input
+          type="text"
+          className="form-control w-fit"
+          id="usernameInput"
+          placeholder="Naam"
+        />
+
+        <input
+          type="text"
+          className="form-control w-fit"
+          id="niveauInput"
+          placeholder="Niveau"
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Aanmaken
+        </button>
+      </form>
+
+      {error && <span className="text-red-400">{error}</span>}
+    </div>
+  );
 }
